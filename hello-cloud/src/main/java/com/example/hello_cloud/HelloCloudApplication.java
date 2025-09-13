8008fc7a9b88cf4f280d441ed4fe2ae727f9b693
“ruben.garruto <r.garruto@reply.it>
2025-09-13 23:49:06 +0200

    Hello World Spring Boot

---
+++ hello-cloud/src/main/java/com/example/hello_cloud/HelloCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCloudApplication.class, args);
+	}
+
+}
---
+++ hello-cloud/src/test/java/com/example/hello_cloud/HelloCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
